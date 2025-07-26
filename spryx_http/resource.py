15136dbb5b48094896d5fb20e593d754b428b4e7
luizhenriquerg <luizshenriquegomess@outlook.com>
2025-07-26 12:48:31 -0300

    chore(project): bump version from 0.2.2 to 0.2.3 and update type hints for client parameters

--- spryx_http/resource.py
+++ spryx_http/resource.py
@@ -1,11 +1,14 @@
-from spryx_http import SpryxAsyncClient, SpryxSyncClient
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from spryx_http import SpryxAsyncClient, SpryxSyncClient
 
 
 class AResource:
-    def __init__(self, client: SpryxAsyncClient):
+    def __init__(self, client: 'SpryxAsyncClient'):
         self._client = client
 
 
 class Resource:
-    def __init__(self, client: SpryxSyncClient):
+    def __init__(self, client: 'SpryxSyncClient'):
         self._client = client
